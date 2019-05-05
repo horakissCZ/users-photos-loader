4aae39decf21321e1d1bbf96a20090c007f50734
Jakub Horák <jakubhorak@outlook.cz>
2019-05-05 23:53:39 +0200

    Add object mapper

--- src/main/java/com/fb/app/UsersPhotosLoaderApplication.java
+++ src/main/java/com/fb/app/UsersPhotosLoaderApplication.java
@@ -1,7 +1,9 @@
 package com.fb.app;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UsersPhotosLoaderApplication {
@@ -9,5 +11,10 @@ public class UsersPhotosLoaderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsersPhotosLoaderApplication.class, args);
 	}
+	
+	@Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
