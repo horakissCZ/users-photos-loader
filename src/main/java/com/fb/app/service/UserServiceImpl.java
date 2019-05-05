93372e9f4848a1b2c033ec4d3e6bb630457d4956
Jakub Horák <jakubhorak@outlook.cz>
2019-05-06 00:18:17 +0200

    Add basic CRUD operations and dto/entity mapping

--- src/main/java/com/fb/app/service/UserServiceImpl.java
+++ src/main/java/com/fb/app/service/UserServiceImpl.java
@@ -1,33 +1,53 @@
 package com.fb.app.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fb.app.domain.User;
 import com.fb.app.dto.PhotoDto;
 import com.fb.app.dto.UserDto;
+import com.fb.app.repository.PhotoRepository;
+import com.fb.app.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
 	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PhotoRepository photoRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@Override
 	public UserDto getUserDetail(String userFbId) {
-		return null;
+		return modelMapper.map(userRepository.findByUserFbId(userFbId), UserDto.class);
 	}
 
 	@Override
 	public List<PhotoDto> getUserPhotos(String userFbId) {
-		return null;
+		return photoRepository.findByUserFbId(userFbId).stream()
+				.map(photo -> modelMapper.map(photo, PhotoDto.class))
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public void createUser(UserDto userDto) {
-		
+		userRepository.save(modelMapper.map(userDto, User.class));
 	}
 
 	@Override
 	public void removeUser(String userFbId) {
+		User foundedUser = userRepository.findByUserFbId(userFbId);
 		
+		if(foundedUser != null)
+			userRepository.delete(foundedUser);
 	}
 
 }
